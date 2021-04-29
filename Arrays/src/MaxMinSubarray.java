dcef9ac8355a2af413165d926c2bdcac7beecb31
Vuong-Chu <minhvuong2992@gmail.com>
2021-04-29 19:09:37 +0100

    Commited

--- Arrays/src/MaxMinSubarray.java
+++ Arrays/src/MaxMinSubarray.java
@@ -1,6 +1,7 @@
-public class MaximumSubarray {
+public class MaxMinSubarray {
     public static void main(String[] args){
         MaxSubarray(new int[]{34,-50,42,14,-5,86});
+        MinSubarray(new int[]{34,-50,42,-49,14,86});
     }
     //Kadane's Algorithm
     public static void MaxSubarray(int[] arr){
@@ -15,5 +16,19 @@ public class MaximumSubarray {
             }
         }
         System.out.println(max);
+    }
+
+    public static void MinSubarray(int[] arr){
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        for(int num: arr) {
+            sum += num;
+            if (sum > 0) {
+                sum = 0;
+            } else {
+                min = Math.min(sum, min);
+            }
+        }
+        System.out.println(min);
     }
 }
